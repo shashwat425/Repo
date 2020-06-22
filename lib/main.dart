79649ab7dc8d4f0466286cbc9818e973ff49082b
shashwat425/ <shashwatjaiswal425@gmail.com>
2020-06-22 22:39:45 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hey!! Doodle'),
+        title: Text('Heyy!! Doodle'),
       ),
       //bottomNavigationBar: BottomNavBar(),
       //body: Xylophone(),
